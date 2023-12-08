e0b0a46c77c8d5ad7e6d0e0e4c33ccb6673ebe70
Oleh Protsak <protsak19@gmail.com>
2023-12-08 22:04:05 +0000

    Implemented a function to color-code time blocks based on whether they are in the past, present, or future.

--- starter/index.js
+++ starter/index.js
@@ -1,1 +1,28 @@
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+
+function updateHourStyles() {
+  let currentHour = dayjs().format("hA");
+  currentHour =
+    currentHour.includes("PM") && parseInt(currentHour) !== 12
+      ? parseInt(currentHour) + 12
+      : parseInt(currentHour);
+
+  $(".time-block").each(function () {
+    const eventBlock = $(this);
+    let blockHour = eventBlock.data("time");
+    blockHour =
+      blockHour.includes("PM") && parseInt(blockHour) !== 12
+        ? parseInt(blockHour) + 12
+        : parseInt(blockHour);
+
+    if (blockHour < currentHour) {
+      eventBlock.removeClass("present future").addClass("past");
+    } else if (blockHour === currentHour) {
+      eventBlock.removeClass("past future").addClass("present");
+    } else {
+      eventBlock.removeClass("present past").addClass("future");
+    }
+  });
+}
+
+updateHourStyles();
