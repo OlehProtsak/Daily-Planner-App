561775360a2258baec341d822fc1387de9e33356
Oleh Protsak <protsak19@gmail.com>
2023-12-09 14:53:34 +0000

    Added comments to the index.js file for better code documentation.

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,7 @@
+// Display the current day at the top of the calendar
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
+// Function to color-code timeblocks based on past, present, or future
 function updateHourStyles() {
   let currentHour = dayjs().format("hA");
   currentHour =
@@ -25,16 +27,20 @@ function updateHourStyles() {
   });
 }
 
+// Call the function to initially set the styles
 updateHourStyles();
 
+// Function to handle saving events to local storage
 $(".saveBtn").on("click", function () {
   const timeblock = $(this).closest(".time-block");
   const hour = timeblock.data("time");
   const eventText = timeblock.find("textarea").val();
 
+  // Save event to local storage
   localStorage.setItem("event_" + hour, eventText);
 });
 
+// Function to load events from local storage
 function loadEvents() {
   $(".time-block").each(function () {
     const hour = $(this).data("time");
@@ -46,6 +52,8 @@ function loadEvents() {
   });
 }
 
+// Call the function to load events when the page loads
 loadEvents();
 
+// Set up an interval to update the styles every minute
 setInterval(updateHourStyles, 60000);
